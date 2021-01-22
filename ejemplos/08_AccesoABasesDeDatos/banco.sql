6756a2264eee7ae110de04605569a9c0304fe788
paco-portada <paco-portada@users.noreply.github.com>
2021-01-22 13:43:34 +0100

    Update banco.sql

--- ejemplos/08_AccesoABasesDeDatos/banco.sql
+++ ejemplos/08_AccesoABasesDeDatos/banco.sql
@@ -22,6 +22,7 @@ SET time_zone = "+00:00";
 -- Base de datos: `banco`
 --
 CREATE DATABASE banco;
+USE banco;
 -- ------------------------------------------------------
 --
 -- Estructura de tabla para la tabla `cliente`
